a411cbb2eb1129aa10f1e34bd14e033471657359
Cedric <cedric.ringger@gmail.com>
2021-12-01 22:08:18 +0100

    Fixed jackson parsing LocalDateTime wrong

--- app/src/main/java/ch/fenix/watschat/models/Message.java
+++ app/src/main/java/ch/fenix/watschat/models/Message.java
@@ -1,12 +1,21 @@
 package ch.fenix.watschat.models;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Message {
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime sentAt;
     private String message;
     private String sender;
+    private String receiverTel;
 }
