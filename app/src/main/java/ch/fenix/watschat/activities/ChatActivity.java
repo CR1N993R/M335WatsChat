e9b587eb125e827f2edab24589ef156501bf280a
cedric <cedric.ringger@gmail.com>
2021-12-02 14:48:10 +0100

    Made the chat updatable from thread

--- app/src/main/java/ch/fenix/watschat/activities/ChatActivity.java
+++ app/src/main/java/ch/fenix/watschat/activities/ChatActivity.java
@@ -8,15 +8,19 @@ import android.content.SharedPreferences;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import ch.fenix.watschat.R;
 import ch.fenix.watschat.adapters.MessageAdapter;
@@ -27,22 +31,33 @@ import ch.fenix.watschat.models.SocketMsg;
 import ch.fenix.watschat.services.SocketService;
 
 public class ChatActivity extends AppCompatActivity {
+    public static ChatActivity chatActivity;
+
     private DataManager dataManager;
     private EditText editText;
     private String tel;
     private SocketService socketService;
+    private ListView content;
+    private MessageAdapter adapter;
+    private static List<Message> messages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        chatActivity = this;
         tel = getIntent().getExtras().getString("contact");
         dataManager = new DataManager(getApplicationContext());
-        ListView content = findViewById(R.id.content);
-        content.setAdapter(new MessageAdapter(getApplicationContext(), R.layout.adapter_view_message, dataManager.getMessagesByTel(tel)));
+        content = findViewById(R.id.content);
+        messages = dataManager.getMessagesByTel(tel);
+        adapter = new MessageAdapter(getApplicationContext(), R.layout.adapter_view_message, messages);
+        content.setAdapter(adapter);
         editText = findViewById(R.id.input);
         findViewById(R.id.send).setOnClickListener(e -> sendMessage());
         bindService();
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
     }
 
     private void bindService(){
@@ -54,6 +69,9 @@ public class ChatActivity extends AppCompatActivity {
         if (editText.getText().toString().length() != 0 && socketService != null) {
             SharedPreferences tel = getApplicationContext().getSharedPreferences("telephone", MODE_PRIVATE);
             Message message = new Message(LocalDateTime.now(), editText.getText().toString(), tel.getString("telephone",""), this.tel);
+            dataManager.saveMessage(message);
+            editText.setText("");
+            refresh();
             ObjectMapper objectMapper = new ObjectMapper();
             try {
                 socketService.sendMessage(objectMapper.writeValueAsString(new SocketMsg(tel.getString("telephone", ""), message)));
@@ -63,8 +81,11 @@ public class ChatActivity extends AppCompatActivity {
         }
     }
 
-    private ServiceConnection connection = new ServiceConnection() {
+    public void updateList() {
+        runOnUiThread(this::refresh);
+    }
 
+    private ServiceConnection connection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName className, IBinder service) {
             SocketService.LocalBinder binder = (SocketService.LocalBinder) service;
@@ -75,4 +96,18 @@ public class ChatActivity extends AppCompatActivity {
         public void onServiceDisconnected(ComponentName arg0) {
         }
     };
+
+    private void refresh() {
+        messages.clear();
+        messages.addAll(dataManager.getMessagesByTel(tel));
+        adapter.notifyDataSetChanged();
+        content.setSelection(adapter.getCount() - 1);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        return super.onOptionsItemSelected(item);
+    }
 }
