ccd41d29f83e2161ae684e0fe926967b36731db2
cedric <cedric.ringger@gmail.com>
2021-12-02 14:43:58 +0100

    Tried fixing bug with listview

--- app/src/main/java/ch/fenix/watschat/adapters/MessageAdapter.java
+++ app/src/main/java/ch/fenix/watschat/adapters/MessageAdapter.java
@@ -1,6 +1,7 @@
 package ch.fenix.watschat.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,7 +37,8 @@ public class MessageAdapter extends ArrayAdapter<Message> {
         textView.setText(getItem(position).getMessage());
         String tel = context.getSharedPreferences("telephone", Context.MODE_PRIVATE).getString("telephone", "");
         if (getItem(position).getSender().equals(tel)) {
-            textView.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+            //TODO Hella buggy
+            textView.setTextColor(Color.BLUE);
         }
         return convertView;
     }
